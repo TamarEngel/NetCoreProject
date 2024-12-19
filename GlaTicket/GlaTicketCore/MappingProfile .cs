117f1dfda830ad2ec51a0e850f22da0e8ae66542
TamarEngel <tamar05861@gmail.com>
2024-12-20 01:59:13 +0200

    DTO & AutoMapper

---
+++ GlaTicket/GlaTicketCore/MappingProfile .cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using GlaTicket.Core.DTO;
+using GlaTicket.Core.models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GlaTicket.Core
+{
+    public class MappingProfile:Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Client,ClientGetDTO>().ReverseMap();
+            CreateMap<Event,EventGetDTO>().ReverseMap();
+            CreateMap<Event,EventPostDTO>().ReverseMap();
+            CreateMap<Event, EventGetOListDTO>().ReverseMap();
+            CreateMap<Producer,ProducerGetDTO>().ReverseMap();
+            CreateMap<Ticket, TicketGetDTO>().ReverseMap();
+            CreateMap<Ticket, TicketPostDTO>().ReverseMap();
+        }
+    }
+}
